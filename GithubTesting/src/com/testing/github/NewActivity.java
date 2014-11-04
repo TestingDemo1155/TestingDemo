e6cdddae2884a7ff7c0aacd751315d4f24b389e1
Karthik.k <Karthik.k@Adeptpros-D-23.adeptpros.com>
2014-11-04 13:07:51 +0530

    MODIFIED THE JAVA

--- GithubTesting/src/com/testing/github/NewActivity.java
+++ GithubTesting/src/com/testing/github/NewActivity.java
@@ -14,11 +14,13 @@ public class NewActivity  extends Activity{
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		 LinearLayout ll=new LinearLayout(NewActivity.this);
-	        setContentView(ll);
+	      
 	    	TextView tv=new TextView(NewActivity.this);
 			Log.e("hi", "hi");
 			tv.setText("hello world");
 			tv.setTextColor(Color.CYAN);
+			ll.addView(tv);
+			  setContentView(ll);
 	}
 	
 }
