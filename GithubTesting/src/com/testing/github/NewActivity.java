0a80fa91a545d23ba651541654233dc2e50f1298
Karthik.k <Karthik.k@Adeptpros-D-23.adeptpros.com>
2014-11-04 12:25:48 +0530

    added onCreate

--- GithubTesting/src/com/testing/github/NewActivity.java
+++ GithubTesting/src/com/testing/github/NewActivity.java
@@ -1,7 +1,16 @@
 package com.testing.github;
 
 import android.app.Activity;
+import android.os.Bundle;
+import android.widget.LinearLayout;
 
 public class NewActivity  extends Activity{
-
+  
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(new LinearLayout(NewActivity.this));
+	}
+	
 }
