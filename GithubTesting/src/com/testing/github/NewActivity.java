dd4cbc9a43598c9223b85368b42c73be5844ef1d
adept-dummy <adept-dummy>
2014-11-04 12:26:54 +0530

    added tv

--- GithubTesting/src/com/testing/github/NewActivity.java
+++ GithubTesting/src/com/testing/github/NewActivity.java
@@ -1,7 +1,18 @@
 package com.testing.github;
 
 import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
 
 public class NewActivity  extends Activity{
 
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		
+		TextView tv=new TextView(NewActivity.this);
+		tv.setText("hello world");
+	}
 }
